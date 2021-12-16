43c40eda4acd5fc574af0dca0bc80fd5fd4cdc9a
Simon Cruanes <simon.cruanes.2007@m4x.org>
2021-12-15 20:54:22 -0500

    make `position` abstract, use index to turn it into a full lexing position

--- lib/pp_loc.mli
+++ lib/pp_loc.mli
@@ -1,5 +1,26 @@
+
+(** A position in a file or string.
+
+    It is abstract to provide more flexibility than {!Lexing.position}.
+
+    @since 2.0
+*)
+module Position : sig
+  type t
+
+  val of_lexing : Lexing.position -> t
+
+  val of_offset : int -> t
+
+  val of_line_col : int -> int -> t
+  (** [of_line_col line col] builds the position indicating the character
+      at column [col] and line [line] of the input.
+      @since 2.0
+  *)
+end
+
 (** The type of source locations: start position, end position *)
-type loc = Lexing.position * Lexing.position
+type loc = Position.t * Position.t
 
 (** Abstraction over the input containing the input to read from. *)
 module Input : sig
@@ -26,17 +47,26 @@ module Input : sig
 
   (** {2 Low-level functions} *)
 
-  (** Creates an input from functions [seek] and [read].
+  (** Creates an input from functions.
 
-      The function [seek] will be called once, before any call to [read_char].
-     If the call to [seek] fails and returns [Error `Invalid_position], the
-     input is considered to be unreadable and will be treated equivalenty as the
-     empty input. This might happen even if the input is valid, if one tries to
-     highlight an invalid location.
+      @param [seek] is a function to move to a given offset in the input.
+      It will be called once, before any call to [read_char].
+      If the call to [seek] fails and returns [Error `Invalid_position], the
+      input is considered to be unreadable and will be treated equivalenty as the
+      empty input. This might happen even if the input is valid, if one tries to
+      highlight an invalid location.
+
+      @param read_char is used to read the next char from the currently seeked
+      position, and seek one char forward.
+
+      @param line_offsets computes the byte offset of each line in the file.
+      This is used when positions are built from only an offset, or only
+      a pair [(line,column)].
   *)
   val raw :
     seek: (int -> (unit, [`Invalid_position]) result) ->
     read_char: (unit -> (char, [`End_of_input]) result) ->
+    line_offsets: (unit -> int array) ->
     t
 
   (** [managed f] enables creating an input which lifetime is managed
